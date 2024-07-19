76e687717c81c340fcc6273d083cfb7c7f0f9f9d
Naman Bansal <namanbansal2016@gmail.com>
2024-07-19 12:57:38 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify, request,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def show_index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
